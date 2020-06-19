4af863c9c869d5b0c8aa924b60359daea4f43ef6
Rafael Justino <rafael.justino@totvs.com.br>
2020-06-19 12:41:59 -0300

    bugfixes

---
+++ README.md
@@ -0,0 +1,54 @@
+
+# Justino SOL Tools Extension (Chrome)
+
+### ⭐ Features
+
+- Calcula a média final;
+- Calcula a N2 mínima pra passar com média 6.0
+
+# O que é?
+**Justino SOL Tools** é uma extensão para o navegador **Google Chrome** facilita o **cálculo da média final** no Sistema Online do aluno (SOL) da PUC-GO.
+
+# Como funciona?
+A extensão extrai os valores das notas de cada disciplina, faz os cálculos e cria uma nova coluna "MF" com as médias finais. Caso tenha já tenha notas N1, a extensão também calcula a nota mínima de N2 que você precisa tirar pra passar na disciplina com média final 6.0.
+
+# Como instalar e configurar?
+A instalação e configuração é bastante simples. Basta seguir os passos abaixo:
+- Fazer o download desse repositório
+- Importar a extensão no Google Chrome
+
+#### Fazer o download desse repositório;
+Estou trabalhando para disponibilizar a extensão lá dentro da Chrome Web Store para facilitar o download e instalação, mas por enquanto você terá que fazer o download e instalação manualmente.
+
+* Clique no botão **Clone or Download** e em seguida em **Download ZIP**
+
+[![](https://i.imgur.com/ZdClmtL.jpg)](https://i.imgur.com/ZdClmtL.jpg)
+
+#### Importar a extensão no Google Chrome
+
+* Após baixar, **extraia** o arquivo ZIP para uma pasta utilizando o [**WinRar**](https://www.winrarbrasil.com.br/winrar/download.mv), [**7ZIP**](https://www.7-zip.org/download.html)   ou qualquer outra ferramenta para extrair arquivos ZIP. 
+
+* Agora no seu Chrome, clique no menu de opções localizado no topo direito da janela (ícone dos 3 pontinhos). Em seguida, vá em **Mais Ferramentas > Extensões**.
+[![](https://i.imgur.com/cYQDjLQ.jpg)](https://i.imgur.com/cYQDjLQ.jpg)
+
+* Habilite o **modo desenvolvedor** clicando no botão do topo superior direito caso não esteja habilitado
+
+[![](https://i.imgur.com/Uyzd3wb.jpg)](https://i.imgur.com/Uyzd3wb.jpg)
+
+* Clique no botão **Carregar sem compactação** e selecione a pasta da que você extraiu anteriormente.
+
+[![](https://i.imgur.com/7TXoaNM.jpg)](https://i.imgur.com/7TXoaNM.jpg)
+
+* Pronto! A extensão foi importada.
+[![](https://i.imgur.com/YJ1cVsC.jpg)](https://i.imgur.com/YJ1cVsC.jpg)
+
+# Como utilizar?
+Essa é a parte mais fácil. Tudo que você precisa fazer é acessar a página das suas notas e frequências no SOL e clicar no ícone da extensão.
+
+![](https://i.imgur.com/o1Jesxv.jpg)
+
+# Atualizações 
+Nesse repositório estarei disponibilizando sempre a versão mais atualizada. Esse projeto foi feito como uma POC. A ideia final é desenvolver um app que conecta com seu SOL e trás muito mais funcionalidades. Já estou trabalhando nisso e pretendo disponibilizá-lo ainda em 2020. Então fique ligado!
+
+Caso encontre algum bug ou tenha alguma dúvida, me mande uma mensagem no meu Instagram ou abra uma Issue. 
+Até breve! 😉
--- script.js
+++ script.js
@@ -15,61 +15,41 @@ function calcular() {
   // Calculando e adicionando as medias
   $('.table-striped > tbody > tr > td:last-child').each(function () {
     
-    
-    let ministradas = $(this).prev().prev().prev().text();
-    let presencas = $(this).prev().prev().text();
+    //let ministradas = $(this).prev().prev().prev().text();
+    //let presencas = $(this).prev().prev().text();
     let n1 = $(this).prev().text();
     let n2 = $(this).text();
 
-    console.log(parseInt(presencas) / parseInt(ministradas) * 100 );
+    if (n1 && n1 !== '-') {
 
-    let minN2 = 0.0;
-    if (isNaN(parseFloat(n2))) {
-      minN2 = (0.4 * parseFloat(n1) - 6) / 0.6 * -1;
-      $(this).text(`min. ${minN2.toFixed(2)}`);
-      $(this).attr('style', 'color: #ff0000; font-weight: bold; font-size:12px');
-    }
+      //console.log(parseInt(presencas) / parseInt(ministradas) * 100 );
+      let minN2 = 0.0;
+      if (isNaN(parseFloat(n2))) {
+        minN2 = (0.4 * parseFloat(n1) - 6) / 0.6 * -1;
 
-    n1 = !isNaN(parseFloat(n1)) ? parseFloat(n1) : 0.0;
-    n2 = !isNaN(parseFloat(n2)) ? parseFloat(n2) : 0.0;
+        $(this).text(`min. ${minN2.toFixed(2)}`);
+        $(this).attr('style', 'color: #ff0000; font-weight: bold; font-size:12px');
+      }
 
-    media = 0.4 * n1 + 0.6 * n2;
+      n1 = !isNaN(parseFloat(n1)) ? parseFloat(n1) : 0.0;
+      n2 = !isNaN(parseFloat(n2)) ? parseFloat(n2) : 0.0;
 
-    let color;
-    if ( media < 6 ) {
-      color = '#ff0000';
+      media = 0.4 * n1 + 0.6 * n2;
+
+      let color;
+      if ( media < 6 ) {
+        color = '#ff0000';
+      } else {
+        color = '##000000';
+      }
+      $(this).parent().append(`<td style="font-size:12px; font-weight: bold; color: ${color}" align="center" >${media.toFixed(2)}</td>`);
+
     } else {
-      color = '##000000';
+      $(this).text(`-`);
+      $(this).prev().text('-');
+      $(this).parent().append(`<td style="font-size:12px;" align="center" >-</td>`);
     }
-    $(this).parent().append(`<td style="font-size:12px; font-weight: bold; color: ${color}" align="center" >${media.toFixed(2)}</td>`);
+    
   });
 
-  
-  /* const audioString = createAudio(nome, 'Você passou!');
-  $('.table-striped').append( audioString );
-  let audio = document.getElementById("au");
-  audio.play(); */
-
-
-  /* $('.table-striped > tbody > tr:last-child').each(function(){
-    var logo = `<img src="${ link }" class="signature1" style="max-height: 180px; max-width: 420px; margin-bottom: -20; ">`;
-    var assinaturaCliente = `<img src="${ linkAssinatura }" class="signature2" style="max-height: 180px; max-width: 420px; margin-bottom: -20; opacity: 0">`;
-    $(this).children().first().prepend(logo);
-    //$(this).children().not(':first').prepend(assinaturaCliente);
-  }); */
-}
-
-function createAudio( nome, resultado ) {
-  console.log('createAudio()');
-  let fraseCompleta = `Olá, ${nome}! ${resultado}`;
-  let fraseResultado = resultado.split(' ').join('-');
-
-  return `<audio id="au" preload="auto" src="https://translate.google.com.vn/translate_tts?ie=UTF-8&q=${fraseCompleta}&tl=pt&client=tw-ob"></audio>`
-}
-
-
-function clearSignatures() {
-  $('.table-striped').each(function(){
-    $(this).remove();
-  });
 }
